cc6e228510536e41707e4d37f0fd53c26b6af402
Alexander Carlson <bombblob@gmail.com>
2019-12-02 00:21:58 -0800

    Added awk version

--- 02/solve.awk
+++ 02/solve.awk
@@ -1,3 +1,51 @@
-END {
+BEGIN {
+	FS = ","
+}
 
+{
+	for (ii = 1; ii <= NF; ii++) {
+		base[ii - 1] = $ii
+	}
+}
+
+func eval_program(base, noun, verb) {
+	for (ii in base) {
+		arr[ii] = base[ii]
+	}
+	arr[1] = noun
+	arr[2] = verb
+	ii = 0
+	done = 0
+	while (done != 1) {
+		switch (arr[ii]) {
+			case 1:
+				a = arr[arr[ii + 1]]
+				b = arr[arr[ii + 2]]
+				arr[arr[ii + 3]] = a + b
+				break
+			case 2:
+				a = arr[arr[ii + 1]]
+				b = arr[arr[ii + 2]]
+				arr[arr[ii + 3]] = a * b
+				break
+			case 99:
+				done = 1
+				break
+		}
+		ii += 4
+	}
+	return arr[0]
+}
+
+END {
+	print "Part one:", eval_program(base, 12, 2)
+
+	for (n = 0; n < 100; n++) {
+		for (v = 0; v < 100; v++) {
+			if (eval_program(base, n, v) == 19690720) {
+				print "Part two:", 100 * n + v
+				exit 0
+			}
+		}
+	}
 }
--- 02/solve.py
+++ 02/solve.py
@@ -2,31 +2,36 @@
 
 import fileinput
 
+def eval_program(arr, noun, verb):
+    arr[1] = noun
+    arr[2] = verb
+    i = 0
+    while True:
+        if arr[i] == 1:
+            a = arr[i + 1]
+            b = arr[i + 2]
+            arr[arr[i + 3]] = arr[a] + arr[b]
+        elif arr[i] == 2:
+            a = arr[i + 1]
+            b = arr[i + 2]
+            arr[arr[i + 3]] = arr[a] * arr[b]
+        elif arr[i] == 99:
+            break
+        else:
+            print(arr[i])
+            print("YIKES")
+        i += 4
+
 for line in fileinput.input():
     copy = [int(n) for n in line.split(",")]
-    # arr += [0 for n in range(10000)]
+
+    arr = [n for n in copy]
+    eval_program(arr, 12, 2)
+    print(arr[0])
+
     for noun in range(100):
         for verb in range(100):
             arr = [n for n in copy]
-            arr[1] = noun
-            arr[2] = verb
-            i = 0
-            while True:
-                if arr[i] == 1:
-                    a = arr[i + 1]
-                    b = arr[i + 2]
-                    arr[arr[i + 3]] = arr[a] + arr[b]
-                elif arr[i] == 2:
-                    a = arr[i + 1]
-                    b = arr[i + 2]
-                    arr[arr[i + 3]] = arr[a] * arr[b]
-                elif arr[i] == 99:
-                    break
-                else:
-                    print(arr[i])
-                    print("YIKES")
-                i += 4
+            eval_program(arr, noun, verb)
             if arr[0] == 19690720:
-                print("N: ", noun, "\n", "V: ", verb)
-    print(arr)
-    print(arr[0])
+                print("N: ", noun, "V: ", verb)
