05c6694eff7220b739f3d88189e228c3900817c6
Alexander Carlson <bombblob@gmail.com>
2019-12-12 23:14:24 -0800

    Got part two

---
+++ 13/readme.txt
@@ -0,0 +1,3 @@
+Part one solution in solve.awk because it is an awk style problem
+
+Part two solution in solve.go and associated go files because I am not recoding my intcode interpreter in awk
---
+++ 13/solve.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func selectVal(a, b int) (r int) {
+	if a == b {
+		return 0
+	} else if a < b {
+		return 1
+	} else {
+		return -1
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	program := read_program(reader)
+
+	program[0] = 2
+
+	progIn := myChan(make(chan int))
+	progOut := myChan(make(chan int))
+
+	go exec_prog(program, progIn, progOut)
+
+	// x,y,val
+	outs := []int{0, 0, 0}
+	score := 0
+
+	screenWH := 50
+	screen := make([][]int, screenWH)
+	for ii := range screen {
+		screen[ii] = make([]int, screenWH)
+	}
+
+	paddleX := 0
+	ballX := 0
+
+	for {
+		open := true
+		select {
+		case outs[0], open = <-progOut:
+			outs[1], open = <-progOut
+			outs[2], open = <-progOut
+			if !open {
+				goto end
+			}
+			if outs[0] == -1 && outs[1] == 0 {
+				score = outs[2]
+			} else {
+				switch outs[2] {
+				case 3:
+					paddleX = outs[0]
+					screen[outs[1]][outs[0]] = outs[2]
+				case 4:
+					ballX = outs[0]
+					screen[outs[1]][outs[0]] = outs[2]
+				default:
+					screen[outs[1]][outs[0]] = outs[2]
+				}
+			}
+		case progIn <- selectVal(paddleX, ballX):
+		}
+
+	}
+end:
+	fmt.Println("Part two:", score)
+}
