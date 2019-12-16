0fcd05f95c982bb0827f56328d82ebf50ff959c6
Alexander Carlson <bombblob@gmail.com>
2019-12-16 00:55:56 -0800

    Done

--- 16/solve.py
+++ 16/solve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import fileinput
+import numpy as np
 
 def run_phase(inList, pattern, times):
     if times <= 0:
@@ -9,11 +10,13 @@ def run_phase(inList, pattern, times):
     patternIdx = 0
     usePattern = pattern
     iterCnt = 1
-    for n in inList:
+    # for n in inList:
+    for n in range(len(inList)):
+        # print(n)
         patternIdx = 1
         num = 0
-        for n2 in inList:
-            num += (n2 * usePattern[patternIdx])
+        for n2 in range(len(inList)):
+            num += (inList[n2] * usePattern[patternIdx])
             patternIdx = (patternIdx + 1) % len(usePattern)
         outList.append(abs(num) % 10)
         usePattern = []
@@ -25,10 +28,22 @@ def run_phase(inList, pattern, times):
 
 line = fileinput.input().__next__()
 inList = [int(n) for n in line if n != '\n']
-# print(inList)
-x = run_phase(inList, [0,1,0,-1], 100)
-print("Part one:", ''.join([str(n) for n in x[0:8]]))
 
-# offset = int(''.join([str(n) for n in inList[0:7]]))
-# print(offset)
-# print(''.join([str(n) for n in x[offset:offset+8]]))
+l1 = run_phase(inList, [0,1,0,-1], 100)
+print("Part one:", ''.join([str(n) for n in l1[0:8]]))
+
+offset = int(''.join([str(n) for n in inList[0:7]]))
+list2 = np.array([n for n in inList] * 10000)
+list2 = list2[offset:]
+
+# for i in range(100):
+#     total = sum(list2)
+#     for j in range(len(list2)):
+#         t = total
+#         total -= list2[j]
+#         list2[j] = abs(t) % 10
+
+for i in range(100):
+    list2 = (list2[::-1].cumsum() % 10)[::-1]
+
+print("Part two:", ''.join([str(n) for n in list2[0:8]]))
