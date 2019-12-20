48002b6fcb6ff4bd57de6ec118a56139069a7418
Alexander <bombblob@gmail.com>
2019-12-19 16:27:19 -0800

    Part two dumb way

--- 19/solve.go
+++ 19/solve.go
@@ -9,8 +9,11 @@ import (
 	intcode "../libintcode"
 )
 
-func pw2(n float64) float64 {
-	return n * n
+func check(x, y int, program []int) int {
+	outArr := []int{}
+	out := intcode.INTCArrWriter{&outArr}
+	intcode.Exec_prog(intcode.Copy_arr(program), &intcode.INTCArr{[]int{x, y}, 0}, out)
+	return outArr[0]
 }
 
 func main() {
@@ -23,26 +26,34 @@ func main() {
 	program := intcode.Read_program(reader)
 	for y := 0; y < 50; y++ {
 		for x := 0; x < 50; x++ {
-			outArr := []int{}
-			out := intcode.INTCArrWriter{&outArr}
-			intcode.Exec_prog(intcode.Copy_arr(program), &intcode.INTCArr{[]int{x, y}, 0}, out)
-			beamMap[y][x] = outArr[0]
-			if outArr[0] == 1 {
+			beamMap[y][x] = check(x, y, program)
+			if beamMap[y][x] == 1 {
 				count++
 			}
 		}
 	}
-	for _, line := range beamMap {
-		for _, c := range line {
-			switch c {
-			case 1:
-				fmt.Printf("O")
-			case 0:
-				fmt.Printf(".")
-			}
-		}
-		fmt.Println()
-	}
+
+	// for _, line := range beamMap {
+	// 	for _, c := range line {
+	// 		switch c {
+	// 		case 1:
+	// 			fmt.Printf("O")
+	// 		case 0:
+	// 			fmt.Printf(".")
+	// 		}
+	// 	}
+	// 	fmt.Println()
+	// }
 
 	fmt.Println("Part one:", count)
+
+	x := 0
+	y := 0
+	for check(x+99, y, program) != 1 {
+		y++
+		for check(x, y+99, program) != 1 {
+			x++
+		}
+	}
+	fmt.Println("Part two:", x*10000+y)
 }
