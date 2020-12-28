fd61a1e7f3f507d8b79c9aa110cb3764ec5b974e
Alexander <bombblob@gmail.com>
2020-12-27 22:02:39 -0800

    25

--- 2020/25/main.hs
+++ 2020/25/main.hs
@@ -6,10 +6,7 @@ genLoops sn = iterate (nextLoop sn) 1
 
 main = do
   inp <- map read . lines <$> readFile "input.txt" :: IO [Int]
-  let (Just a) = elemIndex (head inp) $ genLoops 7
-      (Just b) = elemIndex (last inp) $ genLoops 7
-  print a
-  print b
-  print $ flip (!!) b $ genLoops (head inp)
+  let (Just a) = elemIndex (last inp) $ genLoops 7
+  print $ flip (!!) a $ genLoops (head inp)
 
 -- < 14643366
